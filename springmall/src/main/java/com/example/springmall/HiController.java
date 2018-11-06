67c24152cf09744801627af012890e2f85bd5575
Administrator <Administrator@MSDN-SPECIAL>
2018-11-06 11:41:24 +0900

    [박형민] DBTEST
    Mapper->[Repository]->Service->Controller-> JSP_VIEW

--- springmall/src/main/java/com/example/springmall/HiController.java
+++ springmall/src/main/java/com/example/springmall/HiController.java
@@ -1,12 +1,14 @@
 package com.example.springmall;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HiController {
 	@RequestMapping("/hi")
-	public void hi() {
+	public String hi() {
 		System.out.println("Hi Spring boot!");
+		return "hi"; // forward -> WEB-INF/jsp/hi.jsp
 	}
 }
